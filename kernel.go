bf41495fc559763cf3891fe247e77a132f45ae18
Akshay Deo <akshay@rainingclouds.com>
2014-10-21 00:10:25 +0530

    - initial commit

---
+++ kernel.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+}
---
+++ routes.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+)
+
+func pushRoutes(r *mux.Router) {
+
+}
